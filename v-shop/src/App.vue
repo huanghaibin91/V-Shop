9c122189f6c329fcd837b114bb75b5f941ba4938
huanghaibin91 <huanghaibin91@126.com>
2017-10-20 20:01:14 +0800

    10/20
    10/20

---
+++ v-shop/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <Slider v-model="value" range></Slider>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+  data () {
+    return {
+      value: [20, 50]
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ v-shop/src/main.js
@@ -0,0 +1,20 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+
+// 引入iview
+import iview from 'iview'
+import 'iview/dist/styles/iview.css'
+Vue.use(iview)
+
+Vue.config.productionTip = false
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  template: '<App/>',
+  components: { App }
+})
